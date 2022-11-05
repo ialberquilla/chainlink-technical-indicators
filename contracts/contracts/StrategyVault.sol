4aabdea1c5a7ab507db07048aaeb596f1015d6dd
Ialberquilla <ivan.alberquilla@gmail.com>
2022-11-05 21:23:37 +0100

    keeper contract

---
+++ contracts/contracts/StrategyVault.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.9;
+
+contract StrategyVault {
+    constructor() public {}
+
+    function rebalance() public {}
+
+    function checkRebalance() public view returns (bool) {
+        return false;
+    }
+}
