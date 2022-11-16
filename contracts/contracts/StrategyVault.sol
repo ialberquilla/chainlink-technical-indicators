22b45c5c3b2516315061b17f9901d24a3d8daf1f
Ialberquilla <ivan.alberquilla@gmail.com>
2022-11-16 18:35:03 +0100

    rebalance

--- contracts/contracts/StrategyVault.sol
+++ contracts/contracts/StrategyVault.sol
@@ -1,15 +1,73 @@
 pragma solidity ^0.8.9;
+import {SmaIndicatorOracle} from "./SmaIndicatorOracle.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 contract StrategyVault {
-    bool isRebalanced = false;
+    string asset_pair;
+    string indicator;
+    string time_frame;
+    uint256 period;
+    SmaIndicatorOracle oracle;
+    string network = "Ethereum";
+    uint256 lastTimestamp;
+    string public status = "long";
+    uint256 upper_limit;
+    uint256 lower_limit;
 
-    constructor() public {}
+    constructor(
+        string memory _asset_pair,
+        string memory _indicator,
+        string memory _time_frame,
+        uint256 _period,
+        address _smaIndicatorOracleAddres,
+        uint256 _upper_limit,
+        uint256 _lower_limit
+    ) public {
+        asset_pair = _asset_pair;
+        indicator = _indicator;
+        time_frame = _time_frame;
+        period = _period;
+        oracle = SmaIndicatorOracle(_smaIndicatorOracleAddres);
+        lastTimestamp = block.timestamp;
+        upper_limit = _upper_limit;
+        lower_limit = _lower_limit;
+    }
+
+    function updateIndicator() public {
+        oracle.requestIndicators(
+            Strings.toString(lastTimestamp),
+            Strings.toString(lastTimestamp + period),
+            asset_pair,
+            network,
+            indicator,
+            time_frame,
+            Strings.toString(period)
+        );
+
+        lastTimestamp = block.timestamp;
+    }
 
     function rebalance() public {
-        isRebalanced = !isRebalanced;
+        uint256 indicator_value = oracle.indicator_value();
+        if (indicator_value > upper_limit) {
+            goLong();
+        }
+
+        if (indicator_value < lower_limit) {
+            goShort();
+        }
     }
 
     function checkRebalance() public view returns (bool) {
-        return !isRebalanced;
+        uint256 indicator_value = oracle.indicator_value();
+        return indicator_value > upper_limit || indicator_value < lower_limit;
+    }
+
+    function goLong() public {
+        status = "short";
+    }
+
+    function goShort() public {
+        status = "long";
     }
 }
--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,18 +1,30 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const SmaIndicatorOracle = await ethers.getContractFactory("SmaIndicatorOracle");
+  const smaIndicatorOracle = await SmaIndicatorOracle.deploy();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await smaIndicatorOracle.deployed();
 
-  await lock.deployed();
+  console.log(`SmaIndicatorOracle deployed to ${smaIndicatorOracle.address}`);
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+
+  const StrategyManager = await ethers.getContractFactory("StrategyManager");
+  const strategyManager = await StrategyManager.deploy(smaIndicatorOracle.address);
+
+  await strategyManager.deployed();
+
+  console.log(`StrategyManager deployed to ${strategyManager.address}`);
+
+  await strategyManager.createStrategy(
+    'ETH/USD',
+    'SMA',
+    '60Min',
+    200,
+    1207,
+    1205
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
